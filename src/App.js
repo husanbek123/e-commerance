c1745824a59e97f1c2d68d87b51e10efaa87550f
Biloliddin_dev <bilol9876@gmail.com>
2023-03-05 22:06:12 +0500

    add Nimadur

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import Layout from './Components/Layout';
-import { Route, Routes } from 'react-router-dom';
-import './App.scss';
-import Home from './Pages/Home';
-import AllUsers from './Pages/Users';
-import Products from './Pages/Products';
-import Categories from './Pages/Categories';
-import BestSellings from './Pages/BestSellings';
-import Modal_Outlet from './Pages/Modal_Outlet';
-import ControlPage from './Pages/Control';
+import Layout from "./Components/Layout";
+import { Route, Routes } from "react-router-dom";
+import "./App.scss";
+import Home from "./Pages/Home";
+import AllUsers from "./Pages/Users";
+import Products from "./Pages/Products";
+import Categories from "./Pages/Categories";
+import BestSellings from "./Pages/BestSellings";
+import Modal_Outlet from "./Pages/Modal_Outlet";
+import ControlPage from "./Pages/Control";
 
 function App() {
   return (
     <div className="App">
       <Layout>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/users' element={<AllUsers />} />
-          <Route path='/products' element={<Products />} >
-            <Route path=':action' element={<Modal_Outlet />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/users" element={<AllUsers />} />
+          <Route path="/products" element={<Products />}>
+            <Route path=":action" element={<Modal_Outlet />} />
           </Route>
-          <Route path='/categories' element={<Categories />} />
-          <Route path='/best-sellings' element={<BestSellings />} />
-          <Route path='/control-panel' element={<ControlPage />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/best-sellings" element={<BestSellings />} />
+          <Route path="/control-panel" element={<ControlPage />} />
         </Routes>
       </Layout>
     </div>
