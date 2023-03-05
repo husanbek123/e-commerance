56696a0975b8ba3aebce336ea6fc8b12983a451f
Biloliddin_dev <bilol9876@gmail.com>
2023-03-05 22:13:49 +0500

    add

--- src/Api/Queries.js
+++ src/Api/Queries.js
@@ -1,44 +1,42 @@
 import { useQuery, useMutation } from "@tanstack/react-query";
-import axios from "axios";
+// import axios from "axios";
 import Instance from "../Utils/Instance";
 
-
 function useGetData(keys, api, options) {
-    return useQuery({
-        queryKey: keys,
-        queryFn: async () => {
-            const data = await Instance.get(api);
-            return data.data;
-        },
-        options
-    })
+  return useQuery({
+    queryKey: keys,
+    queryFn: async () => {
+      const data = await Instance.get(api);
+      return data.data;
+    },
+    options,
+  });
 }
 
 function usePostData() {
-    return useMutation({
-        mutationFn: async (api, data) => {
-            const res = await Instance.post(api, data);
-            return res.data;
-        }
-    })
+  return useMutation({
+    mutationFn: async (api, data) => {
+      const res = await Instance.post(api, data);
+      return res.data;
+    },
+  });
 }
 
 function useDeleteData() {
-    return useMutation({
-        mutationFn: (api) => {
-            return Instance.delete(api).then(res => res.data)
-        }
-    })
+  return useMutation({
+    mutationFn: (api) => {
+      return Instance.delete(api).then((res) => res.data);
+    },
+  });
 }
 
 function useUpdateData() {
-    return useMutation({
-        mutationFn: (api, data) => {
-            return Instance.patch(api, data).then(res => res.data)
-        }
-    })
+  return useMutation({
+    mutationFn: (api, data) => {
+      return Instance.patch(api, data).then((res) => res.data);
+    },
+  });
 }
 
-
-export { usePostData , useDeleteData, useUpdateData }
-export default useGetData
+export { usePostData, useDeleteData, useUpdateData };
+export default useGetData;
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import useGetData, { useDeleteData, usePostData} from '../../Api/Queries'
-import styles from './index.module.scss'
+import React from "react";
+import useGetData, { useDeleteData, usePostData } from "../../Api/Queries";
+import styles from "./index.module.scss";
 
 function Home() {
+  // console.log(useGetData(["users"], "https://jsonplaceholder.typicode.com/users").data);
+  let D = useDeleteData();
 
-    // console.log(useGetData(["users"], "https://jsonplaceholder.typicode.com/users").data);
-    let D = useDeleteData()
-
-    function Do() {
-        console.log(D.mutate("https://jsonplaceholder.typicode.com/posts/1"));
-    }
+  function Do() {
+    console.log(D.mutate("https://jsonplaceholder.typicode.com/posts/1"));
+  }
   return (
     <div className={styles.home}>
-        <h3>Hello this is Dashboard</h3>
-        <button onClick={Do}>Submit</button>
+      <h3>Hello this is Dashboard</h3>
+      <button onClick={Do}>Submit</button>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
