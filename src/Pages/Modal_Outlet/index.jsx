f997954f9893c8e9194aedb5a270d014e5fc8f0e
husanbek123 <husanbek555@gmail.com>
2023-03-06 18:33:10 +0500

    Husan321

--- src/Pages/Modal_Outlet/index.jsx
+++ src/Pages/Modal_Outlet/index.jsx
@@ -5,20 +5,23 @@ import { useNavigate, useParams } from 'react-router-dom'
 import MyUpload from '../../Components/Upload';
 import styles from './index.module.scss'
 import { useForm } from 'react-hook-form'; 
-import useGetData, { useDeleteData, usePostData } from '../../Api/Queries';
-
+import useGetData, { useDeleteData, usePostData, useUpdateData } from '../../Api/Queries';
 
 
 
 function Modal_Outlet({type}) {
-  
   let {action} = useParams()
   let navigate = useNavigate()
   let [image, setImage] = useState(null)
 
   let {data: categories} = useGetData(["categories"], "/category")
+  let {data: product} = useGetData(['product/', action], `/products/${action}`)
+  let {data: products} = useGetData(['products'], `/products`)
   let Post = usePostData(`/${type}`)
-  // let Delete = useDeleteData("/category")
+  let Update = useUpdateData(`/products/${action}`)
+
+
+  let CurrentProduct = products?.data?.find((e) => e.id == action)
 
   function Change(e) {
     let file = e.file
@@ -31,30 +34,47 @@ function Modal_Outlet({type}) {
     //   onSuccess: () => console.log("success"),
     // })
 
-    if(type == "products") {
-      // if() 
-    }
+    console.log(values, ";kajsdhjalsjd");
 
-    // Post.mutate({
-    //   "id": "",
-    //   "gender": "BOTH",
-    //   "color": "red",
-    //   "active": true,
-    //   "price": 10,
-    //   "size": "big",
-    //   "type": "string",
-    //   "name_Uz": values.name_Uz,
-    //   "name_Ru": values.name_Ru,
-    //   "name_En": values.name_En,
-    //   "description_Uz": values.description_Uz,
-    //   "description_Ru": values.description_Ru,
-    //   "description_En": values.description_En,
-    //   "photoId": null,
-    //   "categoryId": values.category,
-    //   "discount": 0,
-    //   "image_Url": values.image_url
-    // })  
-    console.log(values);  
+    if(type == "products") {
+      if(action == "add") {
+        Post.mutate({
+          "id": "",
+          "gender": "BOTH",
+          "color": values.color,
+          "active": true,
+          "price": values.price,
+          "size": values.size,
+          "type": "string",
+          "name_Uz": values.name_Uz,
+          "name_Ru": values.name_Ru,
+          "name_En": values.name_En,
+          "description_Uz": values.description_Uz,
+          "description_Ru": values.description_Ru,
+          "description_En": values.description_En,
+          "photoId": null,
+          "categoryId": values.category,
+          "discount": 0,
+          "image_Url": values.image_url
+        }) 
+      }
+      else {
+        Update.mutate({
+          "id": "",
+          "color": values.color,
+          "active": true,
+          "price": Number(values.price),
+          "size": values.size,
+          "name_Uz": values.name_Uz,
+          "name_Ru": values.name_Ru,
+          "name_En": values.name_En,
+          "description_Uz": values.description_Uz,
+          "description_Ru": values.description_Ru,
+          "description_En": values.description_En,
+          "image_Url": values.image_url
+        })
+      }
+    }
   }
 
   return (
@@ -62,21 +82,23 @@ function Modal_Outlet({type}) {
       <div className={styles.closeArea} onClick={() => navigate(-1)}></div>
       <div className={styles.modal}> 
         <Form onFinish={Submit}>
-          <Form.Item  className={styles.input} name='name_Uz' ><Input required placeholder="Enter product's title in uzbek" /></Form.Item>
-          <Form.Item  className={styles.input} name='name_Ru' ><Input required placeholder="Enter product's title in russian" /></Form.Item>
-          <Form.Item  className={styles.input} name='name_En' ><Input required placeholder="Enter product's title in english" /></Form.Item>
-          <Form.Item  className={styles.input} name='price' ><Input required placeholder="Enter product's price" /></Form.Item>
-          <Form.Item  className={styles.textarea} name='description_Uz' ><TextArea required placeholder="Enter product's description in uzbek" /></Form.Item>
-          <Form.Item  className={styles.textarea} name='description_Ru' ><TextArea required placeholder="Enter product's description in russian" /></Form.Item>
-          <Form.Item  className={styles.textarea} name='description_En' ><TextArea required placeholder="Enter product's description in english" /></Form.Item>
+          <Form.Item  className={styles.input} name='name_Uz' initialValue={CurrentProduct?.name_Uz}><Input required placeholder="Enter product's title in uzbek" /></Form.Item>
+          <Form.Item  className={styles.input} name='name_Ru' initialValue={CurrentProduct?.name_Ru}><Input required placeholder="Enter product's title in russian" /></Form.Item>
+          <Form.Item  className={styles.input} name='name_En' initialValue={CurrentProduct?.name_En}><Input required placeholder="Enter product's title in english" /></Form.Item>
+          <Form.Item  className={styles.input} name='price' initialValue={CurrentProduct?.price}><Input required placeholder="Enter product's price" /></Form.Item>
+          <Form.Item  className={styles.input} name='size' initialValue={CurrentProduct?.size}><Input required placeholder="Enter product's size" /></Form.Item>
+          <Form.Item  className={styles.input} name='color' initialValue={CurrentProduct?.color}><Input required placeholder="Enter product's color" /></Form.Item>
+          <Form.Item  className={styles.textarea} name='description_Uz' initialValue={CurrentProduct?.description_Uz}><TextArea required placeholder="Enter product's description in uzbek" /></Form.Item>
+          <Form.Item  className={styles.textarea} name='description_Ru' initialValue={CurrentProduct?.description_Ru} ><TextArea required placeholder="Enter product's description in russian" /></Form.Item>
+          <Form.Item  className={styles.textarea} name='description_En' initialValue={CurrentProduct?.description_En}><TextArea required placeholder="Enter product's description in english" /></Form.Item>
           
-          <Form.Item 
+          {/* <Form.Item 
             name="category"
             className={styles.select}
             required
-          >
-            <Select  
-            
+            initialValue={product?.categoryId}
+          > */}
+            <Select
               showSearch
               className={styles.select}
               placeholder="Choose category"
@@ -87,13 +109,12 @@ function Modal_Outlet({type}) {
               }
               options={categories?.data?.map(item => ({
                 label: item?.name_Uz,
-                value: item?.id
+                value: item?.id,
               }))}
             />
-          </Form.Item>
-          {/* <MyUpload onChange={Change} /> */}
+          {/* </Form.Item> */}
           <Form.Item required className={styles.textarea} name="image_url"><Input placeholder='url for image' /></Form.Item>
-          <Button htmlType='submit' type='primary'>{action.split("_").reverse().join(" ").charAt().toUpperCase() + action.split("_").reverse().join(" ").slice(1)}</Button>
+          <Button htmlType='submit' type='primary'>{action == "add" ? "Add" : "Update"}</Button>
         </Form>
       </div>
     </>
