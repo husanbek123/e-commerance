3e34965aa9c51cf806bfadfe15598f2d752bc61e
Biloliddin_dev <bilol9876@gmail.com>
2023-03-06 14:41:40 +0500

    add nuber coategory

--- src/Pages/Categories/index.jsx
+++ src/Pages/Categories/index.jsx
@@ -1,27 +1,27 @@
-import { Button } from 'antd'
-import React from 'react'
-import { Outlet, useNavigate } from 'react-router-dom'
-import useGetData from '../../Api/Queries'
-import MyTable from '../../Components/Table'
-import styles from './index.module.scss'
-
+import { Button } from "antd";
+import React from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import useGetData from "../../Api/Queries";
+import MyTable from "../../Components/Table";
+import styles from "./index.module.scss";
 
 function Categories() {
-  
-  let navigate = useNavigate()
-  let {data} = useGetData(["categories"], "/category")
+  let navigate = useNavigate();
+  let { data } = useGetData(["categories"], "/category");
 
   return (
     <div className={styles.categories}>
-      <header className='row'>
+      <header className="row">
         <h3>All Categories shu yerda boladi</h3>
-        <Button onClick={() => navigate("/")} type="primary">Add new Category</Button>
+        <Button onClick={() => navigate("/")} type="primary">
+          Add new Category
+        </Button>
       </header>
       <br />
       <Outlet />
 
-      <MyTable data={data?.data}  />
+      <MyTable data={data?.data} />
     </div>
-  )
+  );
 }
-export default Categories
+export default Categories;
